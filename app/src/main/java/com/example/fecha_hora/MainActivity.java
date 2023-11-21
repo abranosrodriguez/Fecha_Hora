53a637f2e74b6abd8df65f36c515e38d52e0d17f
abranosrodriguez <abranosrodriguez@gmail.com>
2023-11-21 18:21:19 +0100

    Ejercicio Corregido
    Corregido errores de escritura

--- app/src/main/java/com/example/fecha_hora/MainActivity.java
+++ app/src/main/java/com/example/fecha_hora/MainActivity.java
@@ -2,20 +2,14 @@ package com.example.fecha_hora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.provider.AlarmClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnRecuperarDatos;
@@ -40,27 +34,7 @@ public class MainActivity extends AppCompatActivity {
                             "\nHora: " + timePicker.getCurrentHour() + ":" + timePicker.getCurrentMinute();
                     Toast.makeText(MainActivity.this, mensajeConDatos, Toast.LENGTH_SHORT).show();
 
-                    /*int hora = timePicker.getCurrentHour();
-                    int minuto = timePicker.getCurrentMinute();
-                    mensaje = "Alarma configurada por aplicacion externa puesta para las: " + hora +":"+minuto;
-                    Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
-                    intent.putExtra(AlarmClock.EXTRA_MESSAGE, mensaje);
-                    intent.putExtra(AlarmClock.EXTRA_HOUR, hora);
-                    intent.putExtra(AlarmClock.EXTRA_MINUTES, minuto);
-
-                    if (intent.resolveActivity(getPackageManager()) == null){
-                        startActivity(intent);
-                    }else{
-                        Toast.makeText(MainActivity.this, "ERROR - NO SE HA CARGADO LA ALARMA", Toast.LENGTH_SHORT).show();
-                    }*/
-
             }
         });
-    }
-
-    public void dias(){
-
-
-
     }
 }
