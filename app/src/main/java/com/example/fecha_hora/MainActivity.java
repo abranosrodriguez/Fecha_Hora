58842b153f93093d85302168081dddf43295493a
abranosrodriguez <abranosrodriguez@gmail.com>
2023-11-21 18:13:17 +0100

    Ejercicio Resuelto

--- app/src/main/java/com/example/fecha_hora/MainActivity.java
+++ app/src/main/java/com/example/fecha_hora/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.fecha_hora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.AlarmClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnRecuperarDatos;
@@ -28,24 +33,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                    mensajeConDatos = "Fecha: " + datePicker.getDayOfMonth() + "/" + datePicker.getMonth() + "/" + datePicker.getYear() +
-                            "\nHora: " + timePicker.getCurrentHour() + "Minuto: " + timePicker.getCurrentMinute();
+                    int dia = datePicker.getDayOfMonth();
+                    int month = datePicker.getMonth();
+                    int year = datePicker.getYear();
+                    mensajeConDatos = "Fecha: " + dia + "/" + month + "/" + year +
+                            "\nHora: " + timePicker.getCurrentHour() + ":" + timePicker.getCurrentMinute();
                     Toast.makeText(MainActivity.this, mensajeConDatos, Toast.LENGTH_SHORT).show();
 
-                    int hora = timePicker.getCurrentHour();
+                    /*int hora = timePicker.getCurrentHour();
                     int minuto = timePicker.getCurrentMinute();
                     mensaje = "Alarma configurada por aplicacion externa puesta para las: " + hora +":"+minuto;
                     Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
                     intent.putExtra(AlarmClock.EXTRA_MESSAGE, mensaje);
                     intent.putExtra(AlarmClock.EXTRA_HOUR, hora);
                     intent.putExtra(AlarmClock.EXTRA_MINUTES, minuto);
+
                     if (intent.resolveActivity(getPackageManager()) == null){
                         startActivity(intent);
                     }else{
                         Toast.makeText(MainActivity.this, "ERROR - NO SE HA CARGADO LA ALARMA", Toast.LENGTH_SHORT).show();
-                    }
+                    }*/
 
             }
         });
+    }
+
+    public void dias(){
+
+
+
     }
 }
